ffee57497489a908b4629a1049a0b93b23b7cd30
Kojo-Brown <nicholasbrown988@gmail.com>
2022-12-13 03:35:02 -1200

    styling  the countries  component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from 'react';
+import { MainRoutes } from './Routes/routes';
+import { Header } from './components/Header';
+import { CountriesContext } from "./Context/countriesContextProvider";
+import { ThemeProvider } from "./Context/themeContextProvider";
+import { ThemeContext } from "./Context/themeContextProvider";
+import Styles from "./styles/componentStyles/style.module.css";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <CountriesContext>
+ <ThemeProvider>
+  <Container>
+     <Header/>
+    <MainRoutes/>
+    </Container>      
+   </ThemeProvider>
+ </CountriesContext>
   );
+}
+
+function Container ({children}:any) {
+  const {theme , setTheme} = useContext(ThemeContext);
+ return ( 
+ <div className={`${Styles[theme]} ${Styles.container}`}>
+  {children}
+  </div>
+  )
 }
 
 export default App;
