d57d8425dd7dd99e2f0393683ea7a99ad3273823
Kojo-Brown <nicholasbrown988@gmail.com>
2022-12-13 02:16:25 -1200

    creating routes

---
+++ src/Routes/routes.tsx
@@ -0,0 +1,8 @@
+import { useRoutes } from "react-router-dom";
+import { Countries } from "../Pages/Countries";
+import { CountryPage } from "../Pages/CountryPage";
+export const MainRoutes = ( ) => {
+    return useRoutes([
+        {path:`/`, element: <Countries />},
+        {path:`/country/:code`, element: <CountryPage/>}])
+}
